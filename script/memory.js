efd1454ee4071982e4bcbe3309bd5a7a0c7cdf54
Unknown <lucie.lesbats@hotmail.fr>
2020-05-23 20:06:41 +0200

    Timer qui change selon le niveau

--- script/memory.js
+++ script/memory.js
@@ -9,7 +9,9 @@ var compte = null;
 
 let niveau = null;
 let nb_cartes = 0;
-//let alea_nb_cartes = 0;
+let temps_jeu = 0;
+let premier_click = true;
+let interval1
 
 function initialiser(evt){
     init_jeu();
@@ -28,19 +30,33 @@ function initialiser(evt){
     }
 }
 
+
+
+function change_deck(evt){
+    console.log("Deck numéro " + evt);
+    var lesCartes = document.querySelectorAll("img");
+    for (var uneCarte of lesCartes){
+        const previous_src = uneCarte.src;
+        const pos = previous_src.indexOf("u");
+        uneCarte.src = previous_src.substr(0, pos+1)+evt+previous_src.substr(pos+2);
+    }
+}
+
+
+
 function init_jeu(){
     const section_niveau = document.getElementById("section_niveau");
     const section_jeu = document.getElementById("section_jeu");
     section_jeu.style.display = "none";
-    document.getElementById("niveau_facile").addEventListener("click", function(){
+    document.getElementById("niveau_facile").addEventListener("click", () => {
         def_niveau("facile")
     });
     
-    document.getElementById("niveau_moyen").addEventListener("click", function(){
+    document.getElementById("niveau_moyen").addEventListener("click", () => {
         def_niveau("moyen")
     });
     
-    document.getElementById("niveau_difficile").addEventListener("click", function(){
+    document.getElementById("niveau_difficile").addEventListener("click", () => {
         def_niveau("difficile")
     });
 }
@@ -50,8 +66,10 @@ function def_niveau(evt){
     section_niveau.style.display = "none";
     section_jeu.style.display = "block";
     maj_nb_cartes();
-    def_temps_jeu();
 }
+
+
+
 
 /* ALEATOIRE A CHANGER ICI */
 /* DETERMINE LE NOMBRE DE CARTES */
@@ -98,28 +116,55 @@ function lancer_jeu(){
     for (var uneCarte of lesCartes){
         uneCarte.addEventListener("click", retournerCarte);
     }
+    def_temps_jeu();
 }
 
-function change_deck(evt){
-    console.log("Deck numéro " + evt);
-    var lesCartes = document.querySelectorAll("img");
-    for (var uneCarte of lesCartes){
-        const previous_src = uneCarte.src;
-        const pos = previous_src.indexOf("u");
-        uneCarte.src = previous_src.substr(0, pos+1)+evt+previous_src.substr(pos+2);
-    }
-}
+
+
+
+
 
 /* ALEATOIRE A CHANGER ICI */
 /* DEFINITION DU TEMPS LIMITE */
 function def_temps_jeu(){
+    if (niveau == "facile"){
+        temps_jeu = 75 + getRandomInt(60); //Entre 75 et 120 sec
+    } else if (niveau == "moyen"){
+        temps_jeu = 60 + getRandomInt(45); //Entre 60 et 105 sec
+    } else if (niveau == "difficile"){
+        temps_jeu = 50 + getRandomInt(55); //Entre 50 et 105 sec
+    } 
+    console.log("Temps jeu 1 : "+temps_jeu);
+}
+
+function lancer_timer(){
+    console.log("Temps jeu 1 : "+temps_jeu);
+    let temps_restant = temps_jeu;
+    interval1 = setInterval(()=>{
+        document.getElementById("timer").innerHTML = temps_restant;
+        temps_restant--;
+        if(temps_restant == 0){
+            window.removeEventListener("click",stop,true);
+            window.alert("Le temps est écoulé, vous avez perdu !");
+            clearInterval(interval1);
+            afficherBouton();
+        }
+    }, 1000);
     
 }
+
+
+
+
 
 function retournerCarte(evt){
     var uneCarte = this;
     uneCarte.classList.add('face');
     uneCarte.removeEventListener("click", retournerCarte);
+    if(premier_click){
+        lancer_timer();
+        premier_click = false;
+    }
     //console.log("message");/*=system.out.printl*/
     if (carte1==null){
         carte1=uneCarte;
@@ -162,7 +207,9 @@ function clonerCartes(evt){
     carte1=null;
     carte2=null;
     window.removeEventListener("click",stop,true);
-    if(document.getElementById("cartesDecouvertes").getElementsByClassName("flip").length==nb_cartes*2){ //remettre à 16
+    if(document.getElementById("cartesDecouvertes").getElementsByClassName("flip").length==nb_cartes*2){ 
+        clearInterval(interval1);
+        window.removeEventListener("click",stop,true);
         window.alert("Vous avez gagné en "+compte+" coups");
         afficherBouton();
     }
